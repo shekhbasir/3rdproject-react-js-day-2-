cf6aea6214114a776a7424ebb1652a24d9b86da3
shekh Basir <shekhbasir9821273349@gmail.com>
2025-07-21 22:56:41 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,56 @@
 function App() {
-  return (
-    <>
-      <h1> this is the code of the new batch in the india </h1>
+  let ink = document.getElementById("root");
 
-      <div class="flex flex-col items-center gap-6 p-7 md:flex-row md:gap-8 rounded-2xl">
-        <div>
-          <img
-            class="size-48 shadow-xl rounded-md"
-            alt=""
-            src="/img/cover.png"
-          />
-        </div>
-        <div class="flex items-center md:items-start">
-          <span class="text-2xl font-medium">Class Warfare</span>
-          <span class="font-medium text-sky-500">The Anti-Patterns</span>
-          <span class="flex gap-2 font-medium text-gray-600 dark:text-gray-400">
-            <span>No. 4</span>
-            <span>·</span>
-            <span>2025</span>
-          </span>
-        </div>
+  function red() {
+    ink.style.backgroundColor = "red";
+  }
+
+  function green() {
+    ink.style.backgroundColor = "green";
+  }
+
+  function black() {
+    ink.style.backgroundColor = "black";
+  }
+
+  function pink() {
+    ink.style.backgroundColor = "pink";
+  }
+  return (
+    <div id="root" style={{ height: 1000, width: 1000 }}>
+      <h1> this is the main code </h1>
+      <br /> <br />
+      <div style={{ backgroundColor: "milk" }}>
+        <button
+          onClick={red}
+          style={{ backgroundColor: "red" }}
+          className=" mr-10"
+        >
+          red
+        </button>
+        <button
+          onClick={green}
+          style={{ backgroundColor: "green" }}
+          className="mr-10"
+        >
+          green
+        </button>
+        <button
+          onClick={black}
+          style={{ backgroundColor: "black" }}
+          className="mr-10"
+        >
+          black
+        </button>
+        <button
+          onClick={pink}
+          style={{ backgroundColor: "pink" }}
+          className="mr-10"
+        >
+          pink
+        </button>
       </div>
-    </>
+    </div>
   );
 }
 export default App;
