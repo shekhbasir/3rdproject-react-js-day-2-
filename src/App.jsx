2365e5b355606396fbfa5121fc3ed250561f1b09
shekh Basir <shekhbasir9821273349@gmail.com>
2025-07-21 22:13:32 +0530

    First commit - React app

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+function App() {
+  return (
+    <>
+      <h1> this is the code of the new batch in the india </h1>
+
+      <div class="flex flex-col items-center gap-6 p-7 md:flex-row md:gap-8 rounded-2xl">
+        <div>
+          <img
+            class="size-48 shadow-xl rounded-md"
+            alt=""
+            src="/img/cover.png"
+          />
+        </div>
+        <div class="flex items-center md:items-start">
+          <span class="text-2xl font-medium">Class Warfare</span>
+          <span class="font-medium text-sky-500">The Anti-Patterns</span>
+          <span class="flex gap-2 font-medium text-gray-600 dark:text-gray-400">
+            <span>No. 4</span>
+            <span>·</span>
+            <span>2025</span>
+          </span>
+        </div>
+      </div>
+    </>
+  );
+}
+export default App;
